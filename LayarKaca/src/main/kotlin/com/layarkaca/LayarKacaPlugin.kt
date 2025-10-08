6ac8909f4bebacee90aaec76c9346d37e0f33c53
Kagem Dhamel <92947791+kagemdhamel@users.noreply.github.com>
2025-10-08 11:05:31 +0700

    Update LayarKacaPlugin.kt

--- LayarKaca/src/main/kotlin/com/layarkaca/LayarKacaPlugin.kt
+++ LayarKaca/src/main/kotlin/com/layarkaca/LayarKacaPlugin.kt
@@ -7,11 +7,8 @@ import com.lagradost.cloudstream3.plugins.Plugin
 @CloudstreamPlugin
 class LayarKacaPlugin : Plugin() {
     override fun load(context: Context) {
-        // All providers should be added in this manner. Please don't edit the providers list
-        // directly.
         registerMainAPI(LayarKaca())
         registerExtractorAPI(Emturbovid())
-        registerExtractorAPI(Furher())
         registerExtractorAPI(Hownetwork())
     }
 }
